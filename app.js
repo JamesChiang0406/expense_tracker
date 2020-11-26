a5eaf6c88502c947a09598829914b8136f9c41cf
James_C <a9356070@gmail.com>
2020-11-26 14:51:56 +0800

    edit function done

--- app.js
+++ app.js
@@ -54,6 +54,37 @@ app.post('/new', (req, res) => {
     .catch(error => console.log(error))
 })
 
+app.get('/:id/edit', (req, res) => {
+  const id = req.params.id
+  res.render('edit', { id })
+})
+
+app.post('/:id/edit', (req, res) => {
+  const id = req.params.id
+  const data = Object.assign(req.body)
+
+  Category.find()
+    .then(type => {
+      return type.find(item => item.name === data.category).icon
+    })
+    .then(type => {
+      data.icon = type
+      return Expense.findById(id)
+        .then(spend => {
+          spend.name = data.name
+          spend.category = data.category
+          spend.date = data.date
+          spend.category = data.category
+          spend.amount = data.amount
+          spend.icon = data.icon
+          return spend.save()
+        })
+    })
+    .then(() => res.redirect('/'))
+    .catch(error => console.log(error))
+
+})
+
 app.listen(port, () => {
   console.log(`Express is listening on https://localhost:${port}`)
 })
