75fe9e4cc018f174f90fd209bfb7c288507360d9
James_C <a9356070@gmail.com>
2020-11-27 16:22:48 +0800

    Heroku init

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const routes = require('./routes')
 require('./config/mongoose')
 
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
@@ -17,5 +17,5 @@ app.use(methodOverride('_method'))
 app.use(routes)
 
 app.listen(port, () => {
-  console.log(`Express is listening on https://localhost:${port}`)
+  console.log(`Express is listening on http://localhost:${port}`)
 })
