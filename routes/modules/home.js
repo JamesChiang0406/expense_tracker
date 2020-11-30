121b000777d5731eb1aaec539c7b43787fd81b57
James_C <a9356070@gmail.com>
2020-11-30 15:52:54 +0800

    Modified all function

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -3,14 +3,13 @@ const router = express.Router()
 const Record = require('../../models/Record')
 
 router.get('/', (req, res) => {
-  const categories = ['家居物業', '交通出行', '休閒娛樂', '餐飲食品', '其他']
+  let totalAmount = 0
 
   Record.find()
     .lean()
     .then(items => {
-      let totalAmount = 0
       items.forEach(spend => totalAmount += spend.amount)
-      res.render('index', { items, totalAmount, categories })
+      res.render('index', { items, totalAmount })
     })
     .catch(error => console.log(error))
 })
--- routes/modules/records.js
+++ routes/modules/records.js
@@ -1,6 +1,5 @@
 const express = require('express')
 const router = express.Router()
-const Category = require('../../models/category')
 const Record = require('../../models/Record')
 
 router.get('/new', (req, res) => {
@@ -10,23 +9,9 @@ router.get('/new', (req, res) => {
 router.post('/new', (req, res) => {
   let data = req.body
 
-  Category.find()
-    .lean()
-    .then(categories => {
-      return categories.find(item => item.name === data.category)
-    })
-    .then(categories => {
-      return Record.create({
-        name: data.name,
-        category: data.category,
-        date: data.date,
-        amount: data.amount,
-        icon: categories.icon
-      })
-    })
+  return Record.create(data)
     .then(() => res.redirect('/'))
     .catch(error => console.log(error))
-
 })
 
 router.get('/:id/edit', (req, res) => {
@@ -41,21 +26,14 @@ router.put('/:id', (req, res) => {
   const id = req.params.id
   const data = req.body
 
-  Category.find()
-    .then(categories => {
-      return categories.find(item => item.name === data.category)
-    })
-    .then(categories => {
+  Record.findById(id)
+    .then(spend => {
       if (data.answer === 'edit') {
-        return Record.findById(id)
-          .then(spend => {
-            spend.name = data.name
-            spend.category = data.category
-            spend.date = data.date
-            spend.amount = data.amount
-            spend.icon = categories.icon
-            return spend.save()
-          })
+        spend.name = data.name
+        spend.date = data.date
+        spend.amount = data.amount
+        spend.icon = data.icon
+        return spend.save()
       }
     })
     .then(() => res.redirect('/'))
@@ -83,18 +61,21 @@ router.delete('/:id', (req, res) => {
     .catch(error => console.log(error))
 })
 
-router.get('/:position', (req, res) => {
-  const position = req.params.position
+router.get('/:icon', (req, res) => {
+  const icon = req.params.icon
+
   Record.find()
     .lean()
     .then(spends => {
-      return spends.filter(spend => spend.category === position)
+      if (icon === 'all') {
+        return spends
+      }
+      return spends.filter(spend => spend.icon === icon)
     })
     .then(items => {
       let totalAmount = 0
       items.forEach(spend => totalAmount += spend.amount)
-      const categories = ['家居物業', '交通出行', '休閒娛樂', '餐飲食品', '其他']
-      res.render('index', { items, totalAmount, categories })
+      res.render('index', { items, totalAmount })
     })
     .catch(error => console.log(error))
 })
