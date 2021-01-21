21e3d2e150f11a73a8b36c52b700bab4e25ea838
James_C <a9356070@gmail.com>
2021-01-21 16:23:39 +0800

    rewrite README & modify login response

--- config/passport.js
+++ config/passport.js
@@ -7,16 +7,16 @@ module.exports = app => {
   app.use(passport.initialize())
   app.use(passport.session())
 
-  passport.use(new LocalStrategy({ usernameField: 'email' }, (email, password, done) => {
+  passport.use(new LocalStrategy({ usernameField: 'email', passReqToCallback: true }, (req, email, password, done) => {
     User.findOne({ email })
       .then(user => {
         if (!user) {
-          return done(null, false, { message: 'This email is not registered!' })
+          return done(null, false, req.flash('warning_msg', '此用戶尚未註冊！'))
         }
         return bcrypt.compare(password, user.password)
           .then(isMatch => {
             if (!isMatch) {
-              return done(null, false, { message: 'Password incorrect!' })
+              return done(null, false, req.flash('warning_msg', '密碼有誤，請再輸入一次！'))
             }
             return done(null, user)
           })
